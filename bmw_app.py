a7a9805e5fa2ec564bd7ab02f93c43d7e6eca77e
raviramani601 <raviramani601@gmail.com>
2025-09-01 10:26:18 +0530

    Update bmw_app.py

--- bmw_app.py
+++ bmw_app.py
@@ -3,7 +3,10 @@ import pandas as pd
 from photo import *
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
-
+# from your main app or wherever you import photo.py
+from photo import set_background
+set_background("C:\Users\LENOVO\PycharmProjects\PythonProject2\.venv/997526.jpg")   # if you stored it in assets/
+# or set_background("997526.jpg") if file is in same folder as photo.py
 # Load and preprocess dataset
 @st.cache_data
 def load_data():
@@ -56,4 +59,5 @@ if st.sidebar.button("Predict Classification"):
     predicted_label = label_encoders['Sales_Classification'].inverse_transform(prediction)
 
     st.success(f"🧠 Predicted Sales Classification: **{predicted_label[0]}**")
+
 
